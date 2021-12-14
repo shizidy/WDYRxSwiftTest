f9900850c899ef43c90ef59ff602c1b3ba23492e
wangdongya <wangdongya1990@163.com>
2021-12-14 15:29:24 +0800

    first commit

---
+++ WDYRxSwiftTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WDYRxSwiftTest
+//
+//  Created by wdyzmx on 2021/12/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .orange
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
